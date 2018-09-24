555e016f2931eb6c97f2a0026fc08d9c831ca793
Mitchell <mitchellfukuzawa@hotmail.com>
2018-09-24 17:35:06 -0400

    Main Menu

---
+++ Game Engine Assignment 1/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Manager : MonoBehaviour {
+
+	public GameObject mainmenu;
+	public GameObject options;
+
+	void Awake()
+	{
+		mainmenu.SetActive(true);
+		options.SetActive(false);
+	}
+
+	public void Play()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
